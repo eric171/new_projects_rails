0a0b5996285847ce98112d3d8078640287e52ca0
Eric Silva <eric.silva10@hotmail.com>
2011-04-04 23:43:50 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Videoaulagit::Application.config.secret_token = 'abeddbc5d1b0cd464927d2768e2d288970606056187e3fa0b31cb5c00c83129d0875d2e3ca77325f569332b2b292e14fea58afa42d8bf5343597d10a41796270'
